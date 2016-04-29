faff511fd7d0db83dfec27ba46b9e3c418ec58b2
Nathan Sowatskey <nathan@nathan.to>
2016-04-29 14:33:52 +0200

    Minor fix.

--- cem-vm/scripts/centos-6.7/install_java_controller.sh
+++ cem-vm/scripts/centos-6.7/install_java_controller.sh
@@ -1,5 +1,4 @@
 sudo yum -y remove java-1.8.0-openjdk-headless.x86_64
-#sudo yum -y install libgcc_s.so.1 glibc.i686
 sudo yum -y install jre-8u91-linux-x64.rpm
 sudo yum -y install EnergyManagement-Controller_5.1_64bit-44163_K9.x86_64.rpm
 
